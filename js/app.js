821f5643b4f16e8dd3963d63ebb4caa07483e600
Daghering <49764315+Daghering@users.noreply.github.com>
2019-10-09 13:50:28 -0400

    initial-commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/*const el = document.getElementsByTagName("input")
+console.log(el)
+el[1].addEventListener("click", function(event){
+    event.preventDefault()
+    console.log(el[0].value)
+    alert("You have subscribed")
+})*/
+
+//const num1 = 9;
+//const num2 = 10;
+
+// if (num1 < num2) {
+//     console.log(`${num1} is less than ${num2}`)
+// } else {
+//     console.log(`${num1} is greater than ${num2}`)
+    
+// }
+
+
+//ternary operator
+//num1< num2 ? console.log("true") : console.log ("false");
